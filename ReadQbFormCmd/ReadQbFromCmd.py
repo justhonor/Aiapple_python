536319befc4a19ed05e6a7ebc9989a85e315fa3b
Aiapple <zx_zhang03@163.com>
2016-04-20 10:53:54 -0400

    修正了正则，url地址；修正了显示代码

--- ReadQbFormCmd/ReadQbFromCmd.py
+++ ReadQbFormCmd/ReadQbFromCmd.py
@@ -17,36 +17,40 @@ class qiubai:
                            'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6',
                            "Accept": "text/plain"
                           }
-                url = "http://www.qiushibaike.com/week/page/%s"%page
-                #url = "http://www.qiushibaike.com"
+		url  = "http://www.qiushibaike.com/8hr/page/%s" %page
                 req = urllib2.Request(url,headers=my_headers)   #以my_headers这种浏览器形式去申请一个对象
                 print "method:",req.get_method()                #使用get还是post呢？
                 resp  = urllib2.urlopen(req)                    #open这个申请对象
                 html  = resp.read()                             #得到html
-                print len(html)                                 
+                print "html len",len(html)                                 
+                print "html type",type(html)                                 
 		#print "eee type of html ",type(html)
                 readDone = time.time()-start
-                html = str(html)                                #将html转换成string类型
-                f = open("html.txt","w+")                       #建立html.txt并将html写入
-                f.writelines(html)
-		f.close()
-                print "type of html  ",type(html)
+                #html = str(html)                                #将html转换成string类型
+               #f = open("html.txt","w+")                       #建立html.txt并将html写入
+               #f.writelines(html)
+		#f.close()
+                #print "type of html  ",type(html)
                 print "after read time%f"%(readDone)
 
 		#建立re_qb这种规则
-                re_qb = re.compile(r'class="content".*?="(.*?)">.*?(\S.*?)\s*?<',re.DOTALL)
+#               re_qb = re.compile(r'class="content".*?="(.*?)">.*?(\S.*?)\s*?<',re.DOTALL)
+                re_qb = re.compile(r'class="content".*?<!--\d',re.DOTALL)
+
                 #用re_qb这种规则去匹配html得到新得my_qiubai
 		my_qiubai = re_qb.findall(html)
+		for line in my_qiubai:
+			print line
                 reDone = time.time()-readDone
                 #print "after re time %f"%(reDone)
-                lenth = len(my_qiubai)
+                #lenth = len(my_qiubai)
                 #ff = open("my_qiubai.txt","w+")                       #建立html.txt并将html写入
                 #ff.writelines(my_qiubai)
 	        #ff.close()
 		#print "my_qiubai is ",my_qiubai
-                for i in range(0,lenth):
-                        for k in range(2):
-                                print my_qiubai[i][k]
+                #for i in range(0,lenth):
+                #        for k in range(2):
+                #                print my_qiubai[i][k]
                 print "after show time %f"%(time.time()-reDone)
                 s = raw_input("回车继续")
                 if s == "q":
--- ReadQbFormCmd/re.py
+++ ReadQbFormCmd/re.py
@@ -3,12 +3,34 @@
 #coding:utf-8
 import re
 f = open("re.file","r")
-html = f.readlines()
-print len(html)
-html = str(html)
-#re_qb = re.compile(r'class="content">',re.DOTALL)
-#re_qb = re.compile(r'<!--\d',re.DOTALL)
-re_qb = re.compile(r'class="content">[\u4e00-\u9fa5]*<!--\d',re.DOTALL)
+html = f.read()
+#html = f.readlines()
+print  "html len:",len(html)
+print  "html type", type(html)
+re_qb = re.compile(r'class="content">.*?<!--\d',re.DOTALL)
+#re_qb = re.compile(r'class="content".*?<!--\d',re.DOTALL)
 my_qiubai = re_qb.findall(html)
-print len(my_qiubai)
+print "my_qiubai type",type(my_qiubai)
+print "my_qiubai len",len(my_qiubai)
+print my_qiubai[0]
+
+my = my.qiubai[0].pop('<')
+print my
+#r = re.compile(r'>.*?<',re.DOTALL)
+#my = r.findall(str(my_qiubai))
+#print "my type",type(my_qiubai)
+#print "my len",len(my_qiubai)
+#print my[0]
 #print my_qiubai
+#for line in my_qiubai:
+        #line = line.rstrip()
+#	print type(line)
+#	print len(line)
+#	print line
+	
+#print my_qiubai
+#<class="content">
+#
+#记得小时候一周岁生日要“抓周”，过年那会儿，正好有一亲戚生孩子，我爸说突然说起我小时候当时一手抓了支铅笔一手抓了支毛笔，大家都说我以后肯定是个>读书的料，可把我爹妈乐坏了。直到很多年后，我才明白，那是二笔的意思。
+#<!--1456393445-->
+
