33df8f0b33572152bd2ebebadc0841f5dc3b4721
aiapple <aiapple@aiapple.(none)>
2017-05-13 15:23:37 +0800

    accomplished version:0.0.1

--- GetMysqlArticle/GetMysqlArticle.py
+++ GetMysqlArticle/GetMysqlArticle.py
@@ -9,81 +9,87 @@
 #############################################
 from bs4 import BeautifulSoup
 import urllib 
-import requests
 import re
 import pdb
 import time
-import os
 
 import sys  
-
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
 main_url = 'http://mysql.taobao.org/monthly/'
-
+start_time = time.time()
 def get_bs_obj(url):
 	fd = urllib.urlopen(url)
 	error = fd.getcode()
 	if error != 200:
 		print "error:%s",error
+	else:
+		print 
+		print 'url: %s open correct'%url
 	html = fd.read()
 	bs = BeautifulSoup(html,"html.parser")
+	#html=''
+	#bs = BeautifulSoup(fd,"html.parser")
+	
 	return	fd,bs,html
 
-#def yunsite(url):
-#    headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-#               'Accept-Encoding': 'gzip, deflate, sdch, br',
-#               'Accept-Language': 'zh-CN,zh;q=0.8',
-#               'Connection': 'keep-alive',
-#               'Host': 'http://mysql.taobao.org/monthly/',
-#               'Upgrade-Insecure-Requests': '1',
-#               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
-#
-#    html = requests.get(url, headers=headers, allow_redirects=False)
-#    return html.headers['Location']
+fd,bs,html = get_bs_obj(main_url)
+article=open('articles.html','wr')
 
-fd,bs,htmll = get_bs_obj(main_url)
-
-article=open('article.html','wr')
-
-
+numbers=0
+# 数据库内核月报
 for a in bs.find_all(name='a',attrs={'href':re.compile(r'monthly/[0-9]')}):
 	# find second_url 	
 	pattern = re.compile(r'[0-9].*?[0-9]$')
 	s_url = pattern.findall(a.attrs.get('href'))
 	second_url = main_url + ''.join(s_url)
-	print second_url
+	#print second_url
 
 	#print yunsite(second_url)
 	cf,cs,html = get_bs_obj(second_url)
-	print "cf status:",cf.getcode()
+	#print "cf status:",cf.getcode()
 	#print html
 
 	title=cs.title.string
-	print "title si:",title	
-	# 数据库内核月报
-	pattern1=re.compile(r'数据库内核月报')
-	if pattern1.findall(title) != [] and 1 == 2:
-		# find  all article url
-		for aa in cs.find_all(name='a',attrs={'href':re.compile(r'monthly/[0-9]')}):
-			pdb.set_trace()
-			# find MySQL article url
-			pattern2=re.compile(r'MySQL')
-			if pattern2.findall(aa.string):
-				patter = re.compile(r'[0-9].*?/')
-				ss_url = patter.findall(aa.attrs.get('href'))
-				arti_url = main_url + ''.join(ss_url)
-			
-				print aa.string,arti_url
-				write_string='<h2><a href=%s>%s</a></h2>\n'%(arti_url,aa.string.strip().replace("·","--"))
-				print write_string
-				article.write(write_string)
-	# 淘宝数据库研发组	
-	pattern3=re.compile(r'淘宝数据库研发组')
-	if pattern3.findall(title) !=[]:
-		pdb.set_trace()
-		# structure the name which consist of ariticles' name
-		print cs.table
+	#print "title si:",title	
+
+	# find  all article url
+	for aa in cs.find_all(name='a',attrs={'href':re.compile(r'monthly/[0-9]')}):
+		# find MySQL article url
+		pattern2=re.compile(r'MySQL')
+		if pattern2.findall(aa.string):
+			patter = re.compile(r'[0-9].*?/')
+			ss_url = patter.findall(aa.attrs.get('href'))
+			arti_url = main_url + ''.join(ss_url)
+		
+			print aa.string.strip()
+			numbers=numbers+1
+			write_string='<h2><a href=%s>%s. %s</a></h2>\n'%(arti_url,numbers,aa.string.strip().replace("·","--"))
+			#print write_string
+			article.write(write_string)
+# 淘宝数据库研发组	
+for t_a in bs.find_all(name='a',attrs={'href':re.compile(r'^.*?php')}):
+	t_url = t_a.attrs.get('href')
+	#print t_url
+
+	t_fd,t_bs,t_html = get_bs_obj(t_url)
+	#print "t_fd status:",t_fd.getcode()
+
+	name=''
+	# structure the name which consist of ariticles' name
+	for names in t_bs.find_all(name='span',attrs={'class':'toctext'}):
+		#pdb.set_trace()
+		name=name+names.string+"--"
+		#print name.strip()
+	name=name[:-2]
+	numbers=numbers+1
+	print name
+	write_string='<h2><a href=%s>%s. %s</a></h2>\n'%(t_url,numbers,name)
+	#print write_string
+	article.write(write_string)
 
 article.close()
+#print start_time,time.time()
+print
+print "The program costs %ss"%(time.time()-start_time)
