ea502e404606992e60a37e6581e5081e026bc811
aiapple <aiapple@aiapple.(none)>
2017-05-23 21:07:49 +0800

    Complete version: 0.0.3 Add logging module to record log information

--- GetMysqlArticle/GetMysqlArticle_ProduceConsume.py
+++ GetMysqlArticle/GetMysqlArticle_ProduceConsume.py
@@ -17,6 +17,21 @@ from Queue import Queue
 from threading import Thread as thread
 import threading
 
+import logging 
+logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s [line:%(lineno)d] %(thread)d %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename='myapp.log',
+                filemode='w')
+
+#Define a StreamHandler object to print INFO level or higher log to screen
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s [line:%(lineno)d] %(thread)d %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+
 import sys  
 reload(sys)  
 sys.setdefaultencoding('utf8')
@@ -50,44 +65,6 @@ def get_bs_obj(url):
 	#html=''
 	#bs = BeautifulSoup(fd,"html.parser")
 	return	fd,bs,html
-
-def Schedule(a,b,c):
-	# a:已经下载的数据块
-	# b:数据块的大小
-	# c:远程文件的大小
-	per = 100.0*a*b/c
-	if per > 100 :
-		per = 100
-	#print '%.2f%%' % per
-
-def Download(data,location):
-	CurrentThreadName = "Consumer ",threading.currentThread().getName()	
-	temp=data
-	url = ''.join(temp.keys())
-
-	name = ''.join(temp[url])
-	if len(name) > 100:
-        	name=name[0:100]+"........."
-	local = os.path.join(location,name)
-	
-	times=0
-	error = 1
-	while error and times < 5:
-		try:
-			print CurrentThreadName,"Downloading :%s"%url
-			print "                              %s "%name
-			urllib.urlretrieve(url,local)
-		except socket.timeout:
-			times=times+1
-			print 'timeout: \n',url
-		except Exception,e:
-			times=times+1
-			print 'error:%s \n'%e
-		else:
-			print "Download correct!\n"
-			error = 0
-		print "Downloading error:%s"%error	
-	
 
 # 数据库内核月报
 def Get_DatabaseReport(bs,fd,html):
@@ -145,7 +122,6 @@ def Get_DatabaseStudy(bs,fd,html):
     	#print write_string
     	article.write(write_string)
    
-
 def DownloadTOLoal():
 	# Download the html to local
 	location='/home/aiapple/git/Aiapple_python/GetMysqlArticle'
@@ -155,7 +131,6 @@ def DownloadTOLoal():
        		#print html
 	finally:
         	f.close()
-
 
 	rest_a={}
 	bs=BeautifulSoup(html,"html.parser")
@@ -195,15 +170,17 @@ def DownloadTOLoal():
 
 def Producer(q,fd,lock):
 	pattern_h=re.compile(r'http://.*?>')
-	pattern_n=re.compile(r'[0-9]+. MySQL.*?<')	
+	pattern_n=re.compile(r'[0-9]+. M.*?SQL.*?<')	
 	pattern_r=re.compile(r'.*?/')
 	
 	L=lock
-	print threading.currentThread().getName(),"Producer starting"
-	CurrentThreadName = "Producer ",threading.currentThread().getName()
+	#print threading.currentThread().getName(),"Producer starting"
+	logging.debug("%s Producer starting"%threading.currentThread().getName())
+	CurrentThreadName = "Producer "+ "%s"%threading.currentThread().getName()
 	while True:
 		if L.acquire():	
-			print CurrentThreadName,"Got lock!!!!"
+			# print CurrentThreadName,"Got lock!!!!"
+			logging.debug("%s Got lock!!!!"%CurrentThreadName)
 			temp={}
 			if fd.closed:
 				L.release()
@@ -211,7 +188,8 @@ def Producer(q,fd,lock):
 			line=fd.readline()
 			# judge whether the file is end
 			if not line : 
-				print "fd at the end of file. return"
+				#print "fd at the end of file. return"
+				logging.debug("%s fd at the end of file. return"%CurrentThreadName)
 				fd.close()
 				L.release()
 				break
@@ -231,21 +209,25 @@ def Producer(q,fd,lock):
 				# put the dict into queue
 				#pdb.set_trace()
 				temp[url]=filename
+				logging.debug("url:%s name:%s"%(url,filename))
+				logging.debug("line:%s "%(line))
 				q.put(temp)
 			L.release()
 		else:
-			print CurrentThreadName,"Can not got lock!!!"
+			#print CurrentThreadName,"Can not got lock!!!"
+			logging.debug("%s Can not got lock!!!"%CurrentThreadName)
 		#time.sleep(0.1)
 
-	print threading.currentThread().getName(),"Producer Ending"
+	#print threading.currentThread().getName(),"Producer Ending"
+	logging.debug("%s Producer ending"%threading.currentThread().getName())
 		
-
 def Consumer(q,fd,lock):
 	# Download html to local with real name
-	print threading.currentThread().getName(),"Consumer Starting"
+	logging.debug("%s Consumer Starting"%threading.currentThread().getName())
+	#print threading.currentThread().getName(),"Consumer Starting"
 	location='/home/aiapple/git/Aiapple_python/GetMysqlArticle'
 
-	CurrentThreadName = "Consumer ",threading.currentThread().getName()	
+	CurrentThreadName = "Consumer "+"%s"%threading.currentThread().getName()	
 	L=lock
 	while True:
 	#	if L.acquire(2):
@@ -253,7 +235,8 @@ def Consumer(q,fd,lock):
 			#pdb.set_trace()
 	#		print CurrentThreadName,"Got lock!!!"
 			if fd.closed and q.empty():
-				print CurrentThreadName," The file is closed and queue is empty.return"
+				logging.debug("%s The file is closed and queue is empty.return"%CurrentThreadName)
+				#print CurrentThreadName," The file is closed and queue is empty.return"
 				#L.release()
 				break
 			else:
@@ -261,15 +244,49 @@ def Consumer(q,fd,lock):
 				# print CurrentThreadName,"geting value from queue"
 				if not q.empty():
 					#pdb.set_trace()
-					print CurrentThreadName," Consumer Download()"
+					logging.debug("%s Consumer Download()"%CurrentThreadName)
+					#print CurrentThreadName," Consumer Download()"
 					Download(q.get(),location) 
 			#L.release()
 	#	else:
 	#		print CurrentThreadName,"Can not got lock!!!"
 			time.sleep(0.1)
 		
-	print threading.currentThread().getName(),"Consumer Ending"
+	#print threading.currentThread().getName(),"Consumer Ending"
+	logging.debug("%s Consumer Ending"%threading.currentThread().getName())
 
+def Download(data,location):
+	CurrentThreadName = "Consumer "+"%s"%threading.currentThread().getName()	
+	temp=data
+	url = ''.join(temp.keys())
+
+	name = ''.join(temp[url])
+	if len(name) > 100:
+        	name=name[0:100]+"........."
+	local = os.path.join(location,name)
+	
+	times=0
+	error = 1
+	while error and times < 5:
+		try:
+			#print CurrentThreadName,"Downloading :%s"%url
+			#print "                              %s "%name
+			logging.debug("%s Downloading :%s"%(CurrentThreadName,url))
+			logging.debug("%s %s "%(CurrentThreadName,name))
+			urllib.urlretrieve(url,local)
+		except socket.timeout:
+			times=times+1
+			#print 'timeout: \n',url
+			logging.debug('timeout: ',url)
+
+		except Exception,e:
+			times=times+1
+			logging.error('%s error:%s url:%s name:%s local:%s data:%s'%(CurrentThreadName,e,url,name,local,data))
+		else:
+			logging.debug("%s Download correct!"%CurrentThreadName)
+			error = 0
+		#print "Downloading error:%s"%error	
+		logging.debug("%s Downloading error:%s\n"%(CurrentThreadName,error))
 
 if __name__=='__main__':
 	
@@ -301,27 +318,30 @@ if __name__=='__main__':
 
 	producers  = [thread(target=Producer,args=(q,fd,P)) for i in xrange(Threads_numbers)]
 	for p in producers:
-		print "starting producer thread %s"%p.getName()
+		#print "starting producer thread %s"%p.getName()
+		logging.debug("starting producer thread %s"%p.getName())
 		p.start()
 
 	consumers  = [thread(target=Consumer,args=(q,fd,C)) for i in xrange(Threads_numbers+2)]
 	for c in consumers:
-		print "starting consumer thread %s"%c.getName()
+		#print "starting consumer thread %s"%c.getName()
+		logging.debug("starting consumer thread %s"%c.getName())
 		c.start()
 
 	for c in consumers:
 		if c.isAlive():
-			print "consumer join()"
+			logging.debug(" consumer join() thread %s"%c.getName())
+			#print "consumer join()"
 			c.join()
 
 	for p in producers:
 		if p.isAlive():
-			print "procuder join()"
+			#print "procuder join()"
+			logging.debug(" producer join() thread %s"%p.getName())
 			p.join()
 
-
-	
-	print
-	print "The program costs %ss"%(time.time()-start_time)
+	#print
+	#print "The program costs %ss"%(time.time()-start_time)
+	logging.debug("The program costs %ss"%(time.time()-start_time))
 	
 
